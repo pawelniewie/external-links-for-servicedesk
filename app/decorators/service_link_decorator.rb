571431fa97ca66cc1f15118776035a548cf4c43d
Pawel Niewiadomski <11110000b@gmail.com>
2016-11-20 22:54:01 +0100

    added encodeURI and encodeURIComponent

--- app/decorators/service_link_decorator.rb
+++ app/decorators/service_link_decorator.rb
@@ -14,6 +14,12 @@ class ServiceLinkDecorator < Draper::Decorator
 
   def render(source)
     handlebars = Handlebars::Context.new
+    handlebars.register_helper(:encodeURIComponent) do |_, context, _|
+      CGI::escape_element(context)
+    end
+    handlebars.register_helper(:encodeURI) do |_, context, _|
+      CGI::escape(context)
+    end
     template = handlebars.compile(source)
     template.call(context[:issue])
   end
