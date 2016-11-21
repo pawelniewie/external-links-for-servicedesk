0322c843c1f7c9511b11b6cc1da19ec92ef58996
Pawel Niewiadomski <11110000b@gmail.com>
2016-11-21 23:21:24 +0100

    renaming service_links to buttons

--- app/controllers/service_buttons_controller.rb
+++ app/controllers/service_buttons_controller.rb
@@ -15,43 +15,43 @@ class ServiceButtonsController < ApplicationController
       format.html do |html|
         html.panel do
           @issue ||= jira_gateway.issue(params[:issue_id])
-          @service_links ||= ServiceLinkDecorator.decorate_collection(
-            current_jwt_auth.service_links.where(project_id: params[:project_id]).ordered,
+          @service_buttons ||= ServiceLinkDecorator.decorate_collection(
+            current_jwt_auth.service_buttons.where(project_id: params[:project_id]).ordered,
             context: {issue: @issue})
         end
       end
       format.json do
-        render json: current_jwt_auth.service_links.where(project_id: params[:project_id]).ordered
+        render json: current_jwt_auth.service_buttons.where(project_id: params[:project_id]).ordered
       end
     end
   end
 
   def create
-    service_link = current_jwt_auth.service_links.create!(
-      params.require(:service_link).permit(:label, :name, :href, :title, :icon).merge(params.permit(:project_id)))
+    service_button = current_jwt_auth.service_buttons.create!(
+      params.require(:service_button).permit(:label, :name, :href, :title, :icon).merge(params.permit(:project_id)))
 
-    render json: service_link
+    render json: service_button
   end
 
   def update
-    current_jwt_auth.service_links
+    current_jwt_auth.service_buttons
       .find_by(params.permit(:project_id, :id))
-      .update(params.require(:service_link).permit(:label, :name, :href, :title, :icon))
+      .update(params.require(:service_button).permit(:label, :name, :href, :title, :icon))
 
-    render json: current_jwt_auth.service_links.find_by(params.permit(:project_id, :id))
+    render json: current_jwt_auth.service_buttons.find_by(params.permit(:project_id, :id))
   end
 
   def destroy
-    current_jwt_auth.service_links.find_by(params.permit(:project_id, :id)).destroy
+    current_jwt_auth.service_buttons.find_by(params.permit(:project_id, :id)).destroy
   end
 
   def move
-    service_link = current_jwt_auth.service_links.find(params[:id])
+    service_button = current_jwt_auth.service_buttons.find(params[:id])
 
     ReorderModelsService.(
       transaction_guard: ServiceButton,
-      moved_object: service_link,
-      all_objects: current_jwt_auth.service_links.where(project_id: service_link.project_id).ordered.to_a,
+      moved_object: service_button,
+      all_objects: current_jwt_auth.service_buttons.where(project_id: service_button.project_id).ordered.to_a,
       params: params)
   end
 
--- db/migrate/20161118233105_add_position_to_service_buttons.rb
+++ db/migrate/20161118233105_add_position_to_service_buttons.rb
@@ -2,7 +2,7 @@ class AddPositionToServiceButtons < ActiveRecord::Migration[5.0]
   def change
     add_column :service_buttons, :position, :integer
     execute <<-SQL.strip_heredoc
-      UPDATE service_links SET position=id
+      UPDATE service_buttons SET position=id
     SQL
     change_column_null :service_buttons, :position, false
   end
