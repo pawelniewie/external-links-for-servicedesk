0107ba01dea2f1d34fb84fb2bf5547604eb42d76
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-01 15:55:46 +0100

    do not initialize table if it's not there

--- app/assets/javascripts/service_buttons.js
+++ app/assets/javascripts/service_buttons.js
@@ -1,31 +1,33 @@
-$(function() {
+$(function () {
   var base_url = $('meta[name=base_url]').attr('content');
   var project_id = $('meta[name=project_id]').attr('content');
 
-  var service_buttons_table = new AJS.RestfulTable({
-    autoFocus: true,
-    el: jQuery("#service_buttons_table"),
-    allowReorder: true,
-    resources: {
-      all: '/projects/' + project_id + '/service_buttons.json',
-      self: '/projects/' + project_id + '/service_buttons'
-    },
-    columns: [
-      {
-        id: "label",
-        header: 'Label'
+  if ($("#service_buttons_table").length > 0) {
+    var service_buttons_table = new AJS.RestfulTable({
+      autoFocus: true,
+      el: jQuery("#service_buttons_table"),
+      allowReorder: true,
+      resources: {
+        all: '/projects/' + project_id + '/service_buttons.json',
+        self: '/projects/' + project_id + '/service_buttons'
       },
-      {
-        id: "href",
-        header: 'Address'
-      },
-      {
-        id: "icon",
-        header: 'Icon'
-      }
-    ]
-  });
+      columns: [
+        {
+          id: "label",
+          header: 'Label'
+        },
+        {
+          id: "href",
+          header: 'Address'
+        },
+        {
+          id: "icon",
+          header: 'Icon'
+        }
+      ]
+    });
 
-  service_buttons_table.bind(AJS.RestfulTable.Events.ROW_INITIALIZED, AP.resize.bind(AP));
-  service_buttons_table.bind(AJS.RestfulTable.Events.INITIALIZED, AP.resize.bind(AP));
+    service_buttons_table.bind(AJS.RestfulTable.Events.ROW_INITIALIZED, AP.resize.bind(AP));
+    service_buttons_table.bind(AJS.RestfulTable.Events.INITIALIZED, AP.resize.bind(AP));
+  }
 });
